7a52a4d5c76508b348a076b8794cb497752144fa
Jackson <jackson.ruby@hotmail.com>
2019-12-18 18:07:29 -0500

    added basic login functionality

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const savePixels = require("save-pixels");
 
 const mongoose = require('mongoose');
 mongoose.set("useFindAndModify", false);
-mongoose.connect('mongodb://localhost:27017/CMS', {
+mongoose.connect('mongodb://localhost:27017/yourpixel', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -42,10 +42,48 @@ myApp.use(session({
 //---------------- Routes ------------------
 
 myApp.get('/',function(req, res){
-    
+    if (req.session.userLoggedIn) {
+        res.render('index');
+    } else {
+        res.redirect('login');
+    }
+});
+
+myApp.get('/login',function(req, res){
+    res.render('login');
+});
+
+myApp.post('/login', [
+    check('username', 'Please enter your username').not().isEmpty(),
+    check('password', 'Please enter your password').not().isEmpty()
+], function(req, res){
+    var user = req.body.username;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        var errorData = {
+            errors: errors.array()
+        };
+        res.render('login', {errorData, user});
+    } else {
+        var pass = req.body.password;
+        Login.findOne({username:user, password:pass}).exec(function(err, foundUser){
+            if (err || foundUser == null) {
+                var error = { msg: 'Username / password combination incorrect'};
+                var errors = [];
+                errors.push(error);
+                var errorData = { errors: error };
+                res.render('login', {errorData, user});
+            } else {
+                req.session.username = foundUser.username;
+                req.session.userLoggedIn = true;
+                req.session.save(function(err){});
+                res.redirect('/');
+            }
+        });
+    }
 });
 
 //----------- Start the server -------------------
 
 myApp.listen(8080);
-console.log('Server started at 8080 for CMS...');
+console.log('Server started at 8080 for yourpixel...');
